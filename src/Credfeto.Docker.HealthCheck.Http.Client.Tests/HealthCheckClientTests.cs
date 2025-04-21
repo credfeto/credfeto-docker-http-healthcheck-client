2454e3f9e08f7d38dbe118bba08395a33a924e12
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-21 21:41:58 +0100

    Started adding in test infrastructure

---
+++ src/Credfeto.Docker.HealthCheck.Http.Client.Tests/HealthCheckClientTests.cs
@@ -0,0 +1,13 @@
+﻿using FunFair.Test.Common;
+using Xunit;
+
+namespace Credfeto.Docker.HealthCheck.Http.Client.Tests;
+
+public sealed class HealthCheckClientTests : TestBase
+{
+    [Fact]
+    public void PlaceHolder()
+    {
+        // Placeholder
+    }
+}
